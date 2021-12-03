d5cb7d1eb25d8ec5e10bfb4821331b6a071dd02b
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2021-12-04 00:16:02 +0530

    resolved few bugs

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,7 +4,7 @@ import ProfilePic from "../public/profile.jpg";
 export default function About() {
   return (
     <div>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <Image
           src={ProfilePic}
           alt="profile image"
@@ -12,8 +12,8 @@ export default function About() {
           height="200px"
           className="rounded"
         />
-        <div class="px-6 py-4">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2 hover:bg-gray-600 hover:text-white">
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-600 mr-2 mb-2 hover:bg-gray-600 hover:text-white">
             About
           </span>
         </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,11 @@ export default function Home() {
         <Image src={background} layout="fill" objectFit="cover" />
       </div>
 
-      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white opacity-90 m-auto">
-        <div class="px-6 py-2">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white opacity-90 m-auto">
+        <div className="px-6 py-2">
           <span>Hey. I'm AX. I'm 23. To know more about me, click About. </span>{" "}
           <br /> <br />
-          <span class=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-600 hover:text-white">
+          <span className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-600 hover:text-white">
             About
           </span>
         </div>
