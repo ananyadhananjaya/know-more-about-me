53940e8f9e3235229c21f9ef51c149124000bdae
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-01-02 20:03:48 +0530

    Final edits for version 1

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,18 +10,18 @@ export default function Home() {
         <Image src={background} layout="fill" objectFit="cover" />
       </div>
 
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white opacity-90 m-auto">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white opacity-80 m-auto">
         <div className="px-6 py-2">
-          <span>Hey. I&apos;m AX. I&apos;m 23. To know more about me, click About. </span>{" "}
-          <br /> <br />
-          <span className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-600 hover:text-white">
-            About
-          </span>
+          <span>Hey. I&apos;m AX. I&apos;m a 23 year old woman. I like learing something new all the time. And, these are my current interestes - </span> <br />
+ <span>1. Quantum Mechanics ( A huge Brian Greene fan )</span> <br />
+ <span>2. Urban Sketching </span> <br />
+ <span>3. Web 3.0  </span><br />
+  <br /> 
         </div>
         <div className="px-6 py-2">
-          <span>Witness my works on Github. </span> <br /> 
-          <span>
-            <Link href="https://github.com/ananyadhananjaya"><a target="_blank"><img  src="/github.svg" alt="github" className="inline-block" /></a></Link>
+          <span>Link to Github &#160;
+          
+            <Link href="https://github.com/ananyadhananjaya"><a target="_blank"><img  src="/github.svg" alt="github" width="30px" height="30px" className="inline-block" /></a></Link>
           </span>
         </div>
       </div>
